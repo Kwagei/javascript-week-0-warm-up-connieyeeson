2a5c0e21950e9970c4a9cec31694ab81c1ea1429
connieyeeson <connieyeeson@gmail.com>
2023-06-01 17:11:35 +0000

    assignment

---
+++ assignment.js
@@ -0,0 +1,59 @@
+
+
+
+// Declare two variables, "x" and "y", and assign them numerical values.
+//  Use the modulus operator to calculate
+//  the remainder when "x" is divided by "y". Print the result.
+
+
+
+    let x = 34;
+    let y = 5;
+    let result = x % y;
+    console.log (result);
+
+
+
+// Problem 2:
+// Declare a variable called "name" and assign it a string value of your choice.
+//  Print the value of the variable to the console.
+
+
+
+let name = "leelos";
+console.log (name);
+
+
+
+// Problem 3:
+// Declare two variables, "num1" and "num2", and assign them any numerical values. 
+// Calculate the sum of the two variables and store the result in a new variable called "sum". 
+// Print the value of the "sum" variable.
+
+let X = 34;
+let Y = 45;
+let sum = X + Y;
+console.log (sum);     
+
+
+
+// Declare a variable called "radius" and assign it a numerical value.
+//  Calculate the area of a circle using the formula A = πr^2 and store the
+
+//   result in a variable called "area". 
+// Print the value of the "area" variable.
+
+let radius = 4^2;
+let A =  3.14
+let area = radius*A
+console.log ( area);
+
+
+
+// Declare a variable called "num" and assign it a numerical value. 
+// Increment the value of "num" by 5 and print the updated value.
+
+ let num = 15;
+ num += 5,
+ console.log(num);
+ 
